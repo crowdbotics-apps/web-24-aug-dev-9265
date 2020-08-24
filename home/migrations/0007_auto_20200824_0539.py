f895d85542fa829e9ae2202d7ad5470e4ce96a74
Crowdbotics <team@crowdbotics.com>
2020-08-24 05:48:52 +0000

    Update models

--- home/migrations/0007_auto_20200824_0539.py
+++ home/migrations/0007_auto_20200824_0539.py
@@ -9,28 +9,56 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('home', '0006_auto_20200824_0536'),
+        ("home", "0006_auto_20200824_0536"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='homepage',
-            name='key1',
-            field=models.ForeignKey(blank=True, default=1, null=True, on_delete=django.db.models.deletion.SET_DEFAULT, related_name='homepage_key1', to=settings.AUTH_USER_MODEL),
+            model_name="homepage",
+            name="key1",
+            field=models.ForeignKey(
+                blank=True,
+                default=1,
+                null=True,
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                related_name="homepage_key1",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AddField(
-            model_name='homepage',
-            name='key2',
-            field=models.ForeignKey(blank=True, default=2, null=True, on_delete=django.db.models.deletion.SET_DEFAULT, related_name='homepage_key2', to='home.CustomText'),
+            model_name="homepage",
+            name="key2",
+            field=models.ForeignKey(
+                blank=True,
+                default=2,
+                null=True,
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                related_name="homepage_key2",
+                to="home.CustomText",
+            ),
         ),
         migrations.AddField(
-            model_name='homepage',
-            name='key3',
-            field=models.OneToOneField(blank=True, default=1, null=True, on_delete=django.db.models.deletion.SET_DEFAULT, related_name='homepage_key3', to=settings.AUTH_USER_MODEL),
+            model_name="homepage",
+            name="key3",
+            field=models.OneToOneField(
+                blank=True,
+                default=1,
+                null=True,
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                related_name="homepage_key3",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AddField(
-            model_name='homepage',
-            name='key4',
-            field=models.OneToOneField(blank=True, default=4, null=True, on_delete=django.db.models.deletion.SET_DEFAULT, related_name='homepage_key4', to=settings.AUTH_USER_MODEL),
+            model_name="homepage",
+            name="key4",
+            field=models.OneToOneField(
+                blank=True,
+                default=4,
+                null=True,
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                related_name="homepage_key4",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
--- home/models.py
+++ home/models.py
@@ -10,50 +10,50 @@ class CustomText(models.Model):
     title = models.CharField(max_length=150,)
     tests = models.OneToOneField(
         "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=1,
         null=True,
         blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
         related_name="customtext_tests",
     )
     user = models.OneToOneField(
         "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=2,
         null=True,
         blank=True,
+        default=2,
+        on_delete=models.SET_DEFAULT,
         related_name="customtext_user",
     )
     new = models.OneToOneField(
         "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=1,
         null=True,
         blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
         related_name="customtext_new",
     )
     test = models.ForeignKey(
         "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=1,
         null=True,
         blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
         related_name="customtext_test",
     )
     key2 = models.ForeignKey(
         "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=1,
         null=True,
         blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
         related_name="customtext_key2",
     )
     key1 = models.ForeignKey(
         "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=2,
         null=True,
         blank=True,
+        default=2,
+        on_delete=models.SET_DEFAULT,
         related_name="customtext_key1",
     )
 
@@ -73,34 +73,34 @@ class HomePage(models.Model):
     body = models.TextField()
     key1 = models.ForeignKey(
         "users.User",
-        on_delete=models.SET_DEFAULT,
-        default=1,
         null=True,
         blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
         related_name="homepage_key1",
     )
     key2 = models.ForeignKey(
         "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=2,
         null=True,
         blank=True,
+        default=2,
+        on_delete=models.SET_DEFAULT,
         related_name="homepage_key2",
     )
     key3 = models.OneToOneField(
         "users.User",
-        on_delete=models.SET_DEFAULT,
-        default=1,
         null=True,
         blank=True,
+        default=1,
+        on_delete=models.SET_DEFAULT,
         related_name="homepage_key3",
     )
     key4 = models.OneToOneField(
         "users.User",
-        on_delete=models.SET_DEFAULT,
-        default=4,
         null=True,
         blank=True,
+        default=4,
+        on_delete=models.SET_DEFAULT,
         related_name="homepage_key4",
     )
 
