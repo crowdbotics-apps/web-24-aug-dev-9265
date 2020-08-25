e3c590196a83dc9cba6926e96ca3943e90d3f55f
Crowdbotics <team@crowdbotics.com>
2020-08-25 11:08:25 +0000

    Update models

--- users/migrations/0003_auto_20200824_0548.py
+++ users/migrations/0003_auto_20200824_0548.py
@@ -7,29 +7,57 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0007_auto_20200824_0539'),
-        ('users', '0002_auto_20200824_0521'),
+        ("home", "0007_auto_20200824_0539"),
+        ("users", "0002_auto_20200824_0521"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='key1',
-            field=models.OneToOneField(blank=True, default=1, null=True, on_delete=django.db.models.deletion.SET_DEFAULT, related_name='user_key1', to='home.CustomText'),
+            model_name="user",
+            name="key1",
+            field=models.OneToOneField(
+                blank=True,
+                default=1,
+                null=True,
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                related_name="user_key1",
+                to="home.CustomText",
+            ),
         ),
         migrations.AddField(
-            model_name='user',
-            name='key2',
-            field=models.OneToOneField(blank=True, default=2, null=True, on_delete=django.db.models.deletion.SET_DEFAULT, related_name='user_key2', to='home.CustomText'),
+            model_name="user",
+            name="key2",
+            field=models.OneToOneField(
+                blank=True,
+                default=2,
+                null=True,
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                related_name="user_key2",
+                to="home.CustomText",
+            ),
         ),
         migrations.AddField(
-            model_name='user',
-            name='key3',
-            field=models.ForeignKey(blank=True, default=3, null=True, on_delete=django.db.models.deletion.SET_DEFAULT, related_name='user_key3', to='home.CustomText'),
+            model_name="user",
+            name="key3",
+            field=models.ForeignKey(
+                blank=True,
+                default=3,
+                null=True,
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                related_name="user_key3",
+                to="home.CustomText",
+            ),
         ),
         migrations.AddField(
-            model_name='user',
-            name='key4',
-            field=models.ForeignKey(blank=True, default=4, null=True, on_delete=django.db.models.deletion.SET_DEFAULT, related_name='user_key4', to='home.CustomText'),
+            model_name="user",
+            name="key4",
+            field=models.ForeignKey(
+                blank=True,
+                default=4,
+                null=True,
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                related_name="user_key4",
+                to="home.CustomText",
+            ),
         ),
     ]
--- users/models.py
+++ users/models.py
@@ -7,38 +7,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=255,)
-    key1 = models.OneToOneField(
-        "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=1,
-        null=True,
-        blank=True,
-        related_name="user_key1",
-    )
-    key2 = models.OneToOneField(
-        "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=2,
-        null=True,
-        blank=True,
-        related_name="user_key2",
-    )
-    key3 = models.ForeignKey(
-        "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=3,
-        null=True,
-        blank=True,
-        related_name="user_key3",
-    )
-    key4 = models.ForeignKey(
-        "home.CustomText",
-        on_delete=models.SET_DEFAULT,
-        default=4,
-        null=True,
-        blank=True,
-        related_name="user_key4",
-    )
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
